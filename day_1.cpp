fed542bc5feb1017188c2e64c1fa6f3c75ba9f8a
yalavrinenko <lavrinenko@phystech.edu>
2020-12-01 13:14:34 +0300

    day 1

---
+++ day_1.cpp
@@ -0,0 +1,34 @@
+//
+// Created by yalavrinenko on 01.12.2020.
+//
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+
+auto part_one(auto v, auto target) {
+  for (auto const &e: v){
+    auto mult2 = std::lower_bound(v.begin(), v.end(), target - e);
+    if (mult2 != v.end() && *mult2 == target  - e)
+      return e * (*mult2);
+  }
+  return 0ll;
+}
+
+auto part_two(auto v, auto target) {
+  for (auto const &e: v){
+    auto double_prod = part_one(v, target - e);
+    if (double_prod != 0)
+      return double_prod * e;
+  }
+  return 0ll;
+}
+
+int main(int argc, char **argv) {
+  std::vector<long long> v;
+  std::copy(std::istream_iterator<long long>{std::cin}, std::istream_iterator<long long>{}, std::back_inserter(v));
+  std::sort(v.begin(), v.end());
+  std::cout << "\nPart 1: " << part_one(v, 2020) << std::endl;
+  std::cout << "\nPart 2: " << part_two(v, 2020) << std::endl;
+  return 0;
+}
