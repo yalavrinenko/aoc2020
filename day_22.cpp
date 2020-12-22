48030244671e8141295bfd1b371d1723a1efe269
yalavrinenko <lavrinenko@phystech.edu>
2020-12-22 13:09:17 +0300

    cange queue to dequeue

--- day_22.cpp
+++ day_22.cpp
@@ -4,7 +4,7 @@
 #include "main.hpp"
 #include <algorithm>
 #include <numeric>
-#include <queue>
+#include <deque>
 #include <ranges>
 #include <set>
 #include <unordered_set>
@@ -15,7 +15,7 @@ struct AOC_Output {
 };
 
 struct AOC_Input {
-  std::queue<long> player_1, player_2;
+  std::deque<long> player_1, player_2;
 };
 
 std::optional<AOC_Input> AOC_Reader::create_from_string(const std::string &line) {
@@ -38,18 +38,17 @@ std::optional<AOC_Input> AOC_Reader::create_from_string(const std::string &line)
     return {};
   }
 
-  if (player == 1) input.player_1.push(std::stol(line));
-  if (player == 2) input.player_2.push(std::stol(line));
+  if (player == 1) input.player_1.emplace_back(std::stol(line));
+  if (player == 2) input.player_2.emplace_back(std::stol(line));
 
   return {};
 }
 
-long deck_score(auto deq) {
+long deck_score(std::deque<long> const &deq) {
+  auto it = deq.begin();
   auto transformed = std::views::iota(1l, static_cast<long>(deq.size()) + 1) | std::views::reverse |
-                     std::views::transform([&deq](auto const &v) {
-                       auto front = deq.front();
-                       deq.pop();
-                       return v * front;
+                     std::views::transform([&it](auto const &v) {
+                       return v * *(it++);
                      });
 
   return {std::reduce(transformed.begin(), transformed.end())};
@@ -61,15 +60,15 @@ AOC_Output part_1(std::vector<AOC_Input> const &v) {
   auto &p2 = clone.player_2;
   while (!p1.empty() && !p2.empty()) {
     auto bet_1 = p1.front();
-    p1.pop();
+    p1.pop_front();
     auto bet_2 = p2.front();
-    p2.pop();
+    p2.pop_front();
     if (bet_1 > bet_2) {
-      p1.push(bet_1);
-      p1.push(bet_2);
+      p1.emplace_back(bet_1);
+      p1.emplace_back(bet_2);
     } else {
-      p2.push(bet_2);
-      p2.push(bet_1);
+      p2.emplace_back(bet_2);
+      p2.emplace_back(bet_1);
     }
   }
 
@@ -78,16 +77,12 @@ AOC_Output part_1(std::vector<AOC_Input> const &v) {
   return {deck_score(nempty)};
 }
 
-auto recursive_card_game(std::queue<long> &p1, std::queue<long> &p2) {
+auto recursive_card_game(std::deque<long> &p1, std::deque<long> &p2) {
   std::set<std::pair<long, long>> states;
 
-  auto create_slice = [](std::queue<long> dec, long n) {
-    std::queue<long> slice;
-    while (n > 0){
-      --n;
-      slice.emplace(dec.front()); dec.pop();
-    }
-    return slice;
+  auto create_slice = [](std::deque<long> const &dec, long n) {
+    auto slice = dec | std::views::take(n);
+    return std::deque(slice.begin(), slice.end());
   };
 
   while (!p1.empty() && !p2.empty()) {
@@ -96,8 +91,8 @@ auto recursive_card_game(std::queue<long> &p1, std::queue<long> &p2) {
     else
       states.insert({state_p1, state_p2});
 
-    auto bet_1 = p1.front();    p1.pop();
-    auto bet_2 = p2.front();    p2.pop();
+    auto bet_1 = p1.front();    p1.pop_front();
+    auto bet_2 = p2.front();    p2.pop_front();
 
     if (bet_1 <= static_cast<long>(p1.size()) && bet_2 <= static_cast<long>(p2.size())) {
       auto dec_slice_1 = create_slice(p1, bet_1);
@@ -105,19 +100,19 @@ auto recursive_card_game(std::queue<long> &p1, std::queue<long> &p2) {
 
       auto winner = recursive_card_game(dec_slice_1, dec_slice_2);
       if (winner == 1) {
-        p1.emplace(bet_1);
-        p1.emplace(bet_2);
+        p1.emplace_back(bet_1);
+        p1.emplace_back(bet_2);
       } else {
-        p2.emplace(bet_2);
-        p2.emplace(bet_1);
+        p2.emplace_back(bet_2);
+        p2.emplace_back(bet_1);
       }
     } else {
       if (bet_1 > bet_2) {
-        p1.emplace(bet_1);
-        p1.emplace(bet_2);
+        p1.emplace_back(bet_1);
+        p1.emplace_back(bet_2);
       } else {
-        p2.emplace(bet_2);
-        p2.emplace(bet_1);
+        p2.emplace_back(bet_2);
+        p2.emplace_back(bet_1);
       }
     }
   }
